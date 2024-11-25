7f28108a682ab8cb105b0c4f2e5c7de79f8c3c0c
NorSaH <167691649+NorSaH77@users.noreply.github.com>
2024-11-25 09:51:45 +0700

    Update b17.cpp

--- b17.cpp
+++ b17.cpp
@@ -4,19 +4,27 @@
 using namespace std;
 
 class Nhanvien {
-    private:
-        string hoten, gioitinh, quequan, chucvu;
-        int namsinh, namlam;
-    public:
-        Nhanvien(string ht="", string cv="", string gt="", string qq="", int ns=0, int nl=0): hoten(ht), chucvu(cv), gioitinh(gt), quequan(qq), namsinh(ns), namlam(nl){}
+private:
+    string hoten, gioitinh, quequan, chucvu;
+    int namsinh, namlam;
+public:
+    Nhanvien(string ht="", string cv="", string gt="", string qq="", int ns=0, int nl=0): hoten(ht), chucvu(cv), gioitinh(gt), quequan(qq), namsinh(ns), namlam(nl) {}
 
-        friend ostream& operator << (ostream& o, const Nhanvien& nv);
+    friend ostream& operator << (ostream& o, const Nhanvien& nv);
 
-        friend istream& operator >> (istream& i, Nhanvien& nv);
+    friend istream& operator >> (istream& i, Nhanvien& nv);
+
+    int getnl() {
+        return 2024-namlam;
+    }
+
+    string getht() {
+        return hoten;
+    }
 };
 
-ostream& operator << (ostream& o, const Nhanvien& nv){
-    o << setw(25) << left << nv.hoten 
+ostream& operator << (ostream& o, const Nhanvien& nv) {
+    o << setw(25) << left << nv.hoten
       << setw(15) << left << nv.chucvu
       << setw(15) << left << nv.gioitinh
       << setw(15) << left << nv.quequan
@@ -26,7 +34,7 @@ ostream& operator << (ostream& o, const Nhanvien& nv){
     return o;
 }
 
-istream& operator >> (istream& i, Nhanvien& nv){
+istream& operator >> (istream& i, Nhanvien& nv) {
     cout << "Nhap ho ten : ";
     getline(i, nv.hoten);
     cout << "Nhap chuc vu :";
@@ -42,27 +50,33 @@ istream& operator >> (istream& i, Nhanvien& nv){
     return i;
 }
 
-int main(){
+int main() {
     const int Nam = 2024;
     int n = 0;
     cout << "Nhap so nhan vien : ";
     cin >> n;
     cin.ignore();
     Nhanvien nv[n];
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++) {
         cout << "Nhap thong tin nhan vien thu " << i + 1 << ":" << endl;
         cin >> nv[i];
         cin.ignore();
     }
-    cout << setw(25) << left << "Ho ten" 
-        << setw(15) << left << "Chuc vu"
-        << setw(15) << left << "Gioi tinh"
-        << setw(15) << left << "Que quan"
-        << setw(15) << left << "Nam sinh"
-        << setw(15) << left << "Nam lam"
-        << endl;
-    for(int i = 0; i < n; i++){
+    cout << setw(25) << left << "Ho ten"
+         << setw(15) << left << "Chuc vu"
+         << setw(15) << left << "Gioi tinh"
+         << setw(15) << left << "Que quan"
+         << setw(15) << left << "Nam sinh"
+         << setw(15) << left << "Nam lam"
+         << endl;
+    for(int i = 0; i < n; i++) {
         cout << nv[i];
+    }
+    cout << "Cac nhan vien lam tren 20 nam :" << endl;
+    for(int i=0; i<n;i++){
+        if(nv[i].getnl()>20){
+            cout<< nv[i].getht() << endl;
+        }
     }
     return 0;
 }
